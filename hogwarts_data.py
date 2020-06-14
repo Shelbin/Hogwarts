d52573dbd5ae89778889a26ce51256aa5fa54c8f
Shelbin <399394409@qq.com>
2020-06-14 19:08:33 +0800

    hogwarts_data文件提交

---
+++ hogwarts_data.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time : 2020/5/12 7:12 下午 
+# @Author :laixiaobin
+# @File : hogwarts_data.py
+
+import sys
+
+a = [1, 2, 3]
+tuple_hogwarts = (1, 2, a)
+print(id(tuple_hogwarts))
+
+print(sys.argv)
+print(sys.path)
